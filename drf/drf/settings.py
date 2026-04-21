fa38633a272715db0e687b543dfd440ca752de93
suyog khadka <ksuyog697@gmail.com>
2026-04-21 23:39:39 +0545

    fixed the issue in the setting.py for config

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config   
+from decouple import config  
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG'),
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
