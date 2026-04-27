991449242bf57dcd3ab1012ad22b4757ad90a22a
suyog khadka <ksuyog697@gmail.com>
2026-04-27 13:25:57 +0545

    added the swagger url

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'pagination',
   'django_filters',
+  'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- drf/pagination/tests.py
+++ drf/pagination/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from rest_framework.test import APIRequestFactory,force_authenticate
+from .views import DetailsViewSet
 # Create your tests here.
+# unit testing --> testing the smallest part of the code 
+# interation testing --> testing one or more than one component at a time (api client ,Api test case )
+
+#API Request factory  --> low- level testing bypass http throtlling 
+
+
+
+factory = APIRequestFactory()
+
+request = factory.get("/pagination/details")
+view = DetailsViewSet.as_view({'get':"list","post":"list"})
+response = view(request)
+
+print(response.status_code)
